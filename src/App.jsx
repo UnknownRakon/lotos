84aa85b78d51f1cde8bb0aef2ae4bdf9e5403a0c
Eiyub Bodur <71319621+jeuub@users.noreply.github.com>
2021-11-20 21:28:39 +0700

    folder naming

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Container from './components/Container/Container';
-import MainPage from './pages/MainPage/MainPage';
 
 const App = () => {
     return (
         <Router>
             <Container>
                 <Header />
-                <Routes>
-                    <Route exact path="/" element={<MainPage />} />
-                </Routes>
             </Container>
         </Router>
     );
