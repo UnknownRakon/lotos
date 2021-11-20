73ce0514a18bd378dc52dec14f3350404ce65ad4
Vlad Tyrtychnyi <tva100702@gmail.com>
2021-11-20 19:57:19 +0300

    feat(Pages): Added Booking Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { HashRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Container from './components/Container/Container';
+import BookingPage from './components/BookingPage/BookingPage';
 
 const App = () => {
     return (
         <Router>
             <Container>
                 <Header />
+                <BookingPage />
             </Container>
         </Router>
     );
