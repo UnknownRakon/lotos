c88a524e5cda9dc37adb6f68d96486ecd0f9ddbb
jeuub <jeuub.bodur@gmail.com>
2022-01-26 18:52:48 +0700

    vk group connected

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,6 +1,15 @@
+import React, { useEffect } from 'react';
 import * as styles from './News.module.scss';
 
 const News = () => {
+    // const [vkLoaded, setVkLoaded] = useState(false);
+    useEffect(() => {
+        window.VK.Widgets.Group(
+            'vk_groups',
+            { mode: 4, width: 'auto', height: '800' },
+            208980239
+        );
+    }, []);
     return (
         <div className={styles.wrapper}>
             <div id="vk_groups" className={styles.vk_group} />
